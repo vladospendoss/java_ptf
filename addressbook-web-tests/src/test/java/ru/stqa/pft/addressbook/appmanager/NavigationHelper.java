700dba61d5e127def72429c86b3384f4b9e6ea5b
Владислав Савенко <vladislavsavenko@Mac-mini-kuku-epta.local>
2018-07-26 16:03:34 +0300

    Рефакторинг
    Насоздавали хэлперов с помощью делегации

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationHelper {
+
+    private WebDriver driver;
+
+    public NavigationHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void gotoGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+}
