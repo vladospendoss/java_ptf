06334b415ed3966c1e129aad412659e4f76b76e1
Владислав Савенко <vladislavsavenko@Mac-mini-kuku-epta.local>
2018-08-22 14:20:07 +0300

    шо-то оч важное
    Добавлили иквелсы и компараторы я хз как это работает, но еще не вечер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,9 +2,13 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupHelper extends HelperBase{
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(WebDriver driver) {
         super(driver);
@@ -24,17 +28,42 @@ public class GroupHelper extends HelperBase{
         type(By.name("group_footer"), groupData.getFooter());
     }
 
-    public void initGroupCreation() { click(By.name("new")); }
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
 
-    public void deleteSelectedGroups() { click(By.name("delete")); }
+    public void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
+    }
 
     public void selectGroup(int index) {
-        driver.findElements(By.name("selected[]")).get(index).click(); }
+        driver.findElements(By.name("selected[]")).get(index).click();
+    }
 
-    public void initGroupModification() { click(By.name("edit")); }
+    public void initGroupModification() {
+        click(By.name("edit"));
+    }
 
-    public void submitGroupModification() { click(By.name("update")); }
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
 
-    public int getGroupCount() { return driver.findElements(By.name("selected[]")).size(); }
-
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            GroupData group = new GroupData(id, name, null, null);
+            groups.add(group);
+        }
+        return groups;
+    }
 }
